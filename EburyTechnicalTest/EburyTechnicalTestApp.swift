036c3063aaed0c2c2b5d22fd88f94d24dc3d831d
Sergio Ulloa <sergio95rlc@gmail.com>
2024-06-24 10:20:31 +0200

    Initial Commit

---
+++ EburyTechnicalTest/EburyTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EburyTechnicalTestApp.swift
+//  EburyTechnicalTest
+//
+//  Created by Sergio Ulloa López on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EburyTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
