81018dd8019ec5e9ebf3e166684ed494aa238f97
Sergio Ulloa <sergio95rlc@gmail.com>
2024-06-25 21:12:37 +0200

    Add gitignore

---
+++ EburyTechnicalTest/App/Network/Endpoint.swift
@@ -0,0 +1,10 @@
+//
+//  Endpoint.swift
+//  EburyTechnicalTest
+//
+//  Created by Sergio Ulloa López on 25/6/24.
+//
+
+protocol Endpoint {
+    var path: String { get }
+    var method: HTTPMethod { get }}
